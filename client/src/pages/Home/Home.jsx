a5fe478ee213cbf137af7dd9854c4455584cb437
gokul <gokulchandan24@gmail.com>
2024-10-07 00:04:29 +0530

    react modal comp added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
 
 const Home = () => {
   return (
@@ -18,38 +20,15 @@ const Home = () => {
             onDelete={() => {}}
             onPinNote={() => {}}
           />
-          <NoteCard
-            title="Meeting on 18th June"
-            date="21st Dec 2024"
-            content="This is one of the very important meetings"
-            tags="#meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
-          <NoteCard
-            title="Meeting on 18th June"
-            date="21st Dec 2024"
-            content="This is one of the very important meetings"
-            tags="#meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
-          <NoteCard
-            title="Meeting on 18th June"
-            date="21st Dec 2024"
-            content="This is one of the very important meetings"
-            tags="#meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
         </div>
       </div>
+      <button
+        className="w-16 h-16 items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px] text-white ml-4" />
+      </button>
+      <AddEditNotes />
     </>
   );
 };
