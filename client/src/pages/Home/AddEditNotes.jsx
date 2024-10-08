98460f96dd243c34b24205149f223f2879c32b9e
gokul <gokulchandan24@gmail.com>
2024-10-09 00:48:09 +0530

    added modal component

--- client/src/pages/Home/AddEditNotes.jsx
+++ client/src/pages/Home/AddEditNotes.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import TagInput from "../../components/Input/TagInput";
+import { MdAdd, MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter the title");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content");
+      return;
+    }
+
+    setError("");
+  };
   return (
     <>
-      <div>
+      <div className="relative">
+        <button
+          className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:text-slate-500"
+          onClick={onClose}
+        >
+          <MdClose className="text-xl text-slate-400" />
+        </button>
         <div className="flex flex-col gap-2">
           <label className="input-label">TITLE</label>
           <input
             type="text"
             className="text-2xl text-slate-500 outline-none"
             placeholder="Go to gym at 5"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
           />
         </div>
         <div className="flex flex-col gap-2 mt-4">
@@ -19,12 +48,19 @@ const AddEditNotes = () => {
             className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
             placeholder="Content"
             rows={10}
+            value={content}
+            onChange={({ target }) => setContent(target.value)}
           />
         </div>
         <div className="mt-3">
           <label className="input-label">TAGS</label>
+          <TagInput tags={tags} setTags={setTags} />
         </div>
-        <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+        {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+        <button
+          className="btn-primary font-medium mt-5 p-3"
+          onClick={handleAddNote}
+        >
           ADD
         </button>
       </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <Navbar />
@@ -24,11 +30,30 @@ const Home = () => {
       </div>
       <button
         className="w-16 h-16 items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
       >
         <MdAdd className="text-[32px] text-white ml-4" />
       </button>
-      <AddEditNotes />
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-hidden"
+      >
+        <AddEditNotes
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
+          }}
+        />
+      </Modal>
     </>
   );
 };
